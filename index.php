e8ad010f273aacbce2b227820e5da046850b7a57
Vainiuks <vainius.daraskevicius@gmail.com>
2022-12-03 23:28:08 +0200

    admin panel update

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ include_once 'classes/product.class.php';
 include_once 'classes/cart.class.php';
 include_once 'classes/filter.class.php';
 
+// Failui istrinti
+// unlink('includes/product_images/1234.jpg');
 
 $productObj = new Product();
 $cartObj = new Cart();
